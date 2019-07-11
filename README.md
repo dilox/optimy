f565f9ebf960e9bbc5893b301546c69985f5bd2a
Pierluigi Di Lorenzo <pierluigi@sciabarra.com>
2019-07-11 23:53:54 +0200

    test should be ok

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # optimy
-test for optimy
+Test for Optimy
+
+- create the following env vars (I'll send the real ones by email, together the private key):
+
+(on linux)
+export AWS_ACCESS_KEY_ID="AKIA................"
+export AWS_SECRET_ACCESS_KEY="+GiNB8D................................."
+
+(on windows cmd)
+set AWS_ACCESS_KEY_ID=AKIA................
+set AWS_SECRET_ACCESS_KEY=+GiNB8D.................................
+
+after cloning this repo (git@github.com:dilox/optimy.git) run:
+
+>terraform init
+>terraform plan
+>terraform apply
+
+you should see as output something like:
+optimy_instance_public_ip = 52.47.194.144
+
+use the ip to open:
+
+http://optimy_instance_public_ip/test.php
--- main.tf
+++ main.tf
@@ -3,54 +3,60 @@ provider "aws" {
 }
 
 resource "aws_instance" "optimy-test" {
-  ami           = "ami-080d4d4c37b0aa206"
-  instance_type = "t2.micro"
-  key_name        = "${aws_key_pair.optimy_key.key_name}"
+  ami                    = "ami-080d4d4c37b0aa206"
+  instance_type          = "t2.micro"
+  key_name               = "${aws_key_pair.optimy_key.key_name}"
   vpc_security_group_ids = [aws_security_group.instance.id]
 
   user_data = <<-EOF
               #!/bin/bash
               sudo apt-get update
               sudo apt-get install -y apache2 php libapache2-mod-php mysql-client-core-5.7 php7.0-mysqli
+              sudo chmod 777 /var/www/html/
+              sudo chmod 777 /etc/php/7.0/apache2/
               echo 'create table optimy(id INT NOT NULL AUTO_INCREMENT, output VARCHAR(100) NOT NULL, PRIMARY KEY(id));' | mysql -uadmin -poptimy123 -h ${aws_db_instance.optimy_db_test.address} optimy_test_database
-              echo 'INSERT INTO optimy(output) values ("Hello World");' | mysql -uadmin -poptimy123 -h ${aws_db_instance.optimy_db_test.address} optimy_test_database
-              
+              echo 'INSERT INTO optimy(output) values ("Hello World");' | mysql -uadmin -poptimy123 -h ${aws_db_instance.optimy_db_test.address} optimy_test_database              
+              echo '<?php
+              $conn = new mysqli("${aws_db_instance.optimy_db_test.address}", "admin", "optimy123", "optimy_test_database");
+              $sql = "SELECT output FROM optimy LIMIT 1";
 
-sudo cat << EOP >> /var/www/html/test.php
-<?php
-$conn = new mysqli("${aws_db_instance.optimy_db_test.address}", "admin", "optimy123", "optimy_test_database");
-$sql = 'SELECT output FROM optimy';
+              $result = mysqli_query($conn, $sql);
 
-$result = mysqli_query($conn, $sql);
-
-while($row = mysqli_fetch_assoc($result)) {
-        echo "<b>" . $row["output"]. "<br>";
-}
-$conn->close();
-?>
-EOP
+              while($row = mysqli_fetch_assoc($result)) {
+              echo "<b>" . $row["output"]. "<br>";
+              }
+              $conn->close();
+              ?>' > /var/www/html/test.php
 EOF
-  
+
+  connection {
+    type = "ssh"
+    host = "${self.public_ip}"
+    user = "ubuntu"
+    private_key = "${file("optimy_key")}"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 30",
+      "sudo chown -R ubuntu:ubuntu /etc/php/7.0/apache2/",
+      "sudo chown -R ubuntu:ubuntu /var/www/html/"
+    ]
+  }
+
+  provisioner "file" {
+    source = "conf/php.ini"
+    destination = "/etc/php/7.0/apache2/php.ini"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sleep 30",
+      "sudo service apache2 restart"
+    ]
+  }
+
   tags = {
     Name = "optimy-test"
   }
 }
-
-
-/* to add to the script
-echo 'create table optimy(id INT NOT NULL AUTO_INCREMENT, output VARCHAR(100) NOT NULL, PRIMARY KEY(id));' | mysql -uadmin -poptimy123 -h ${var.db_address} optimy_test_database
-echo 'INSERT INTO optimy(output) values ("Hello World");' | mysql -uadmin -poptimy123 -h ${var.db_address} optimy_test_database
-
-<?php
-$conn = new mysqli("optimy20190711171227448300000001.cvmtxy3vixlb.eu-west-3.rds.amazonaws.com", "admin", "optimy123", "optimy_test_database");
-$sql = 'SELECT output FROM optimy';
-
-$result = mysqli_query($conn, $sql);
-
-while($row = mysqli_fetch_assoc($result)) {
-        echo "<b>" . $row["output"]. "<br>";
-}
-$conn->close();
-?>
-*/
-  
